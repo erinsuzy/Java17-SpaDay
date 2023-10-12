1683fe0a331ac9748d925961f8a3aff12c0e148b
Erin.Schneider <erinsuzy@gmail.com>
2023-10-11 19:47:00 -0500

    add form

--- src/main/java/org/launchcode/controllers/UserController.java
+++ src/main/java/org/launchcode/controllers/UserController.java
@@ -1,15 +1,24 @@
 package org.launchcode.controllers;
 
+import org.launchcode.models.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
 
-    @GetMapping("/user/add")
+    @GetMapping("/add")
     public String displayAddUserForm(){
         return "/user/add.html";
+    }
+
+   @PostMapping("/user")
+    public String processAddUserForm(Model model, @ModelAttribute User user, String verify) {
+
     }
 }
