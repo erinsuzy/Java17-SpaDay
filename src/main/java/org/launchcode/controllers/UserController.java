ce45c0a83034b28cf9b11cf32438356fbd07fc8c
Erin.Schneider <erinsuzy@gmail.com>
2023-10-11 19:59:33 -0500

    controller stuff

--- src/main/java/org/launchcode/controllers/UserController.java
+++ src/main/java/org/launchcode/controllers/UserController.java
@@ -8,17 +8,22 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Objects;
+
 @Controller
 @RequestMapping("/user")
 public class UserController {
 
     @GetMapping("/add")
     public String displayAddUserForm(){
-        return "/user/add.html";
+        return "/user/add";
     }
 
    @PostMapping("/user")
     public String processAddUserForm(Model model, @ModelAttribute User user, String verify) {
-
+        if(Objects.equals(verify, user.getPassword())){
+            System.out.println("Welcome " + user.getUsername());
+        }
+        return "/user/add";
     }
 }
